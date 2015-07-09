875bc185ba611e1a54dbe19a195baf6611d3d11b
Paul Seidel <pse@ableton.com>
2015-07-09 16:52:25 +0200

    Show character information

--- gw2_api.nim
+++ gw2_api.nim
@@ -1,27 +1,52 @@
-import httpclient
-import uri
-import json
-import strutils
-import sequtils
+from httpclient import request
+from uri import parseUri, `/`, `$`
+from json import items, JsonNode, parseJson, `[]`
+from strutils import join, replace, `%`
+from sequtils import mapIt
 
 const apiURL = "https://api.guildwars2.com/v2/"
 
-proc buildRequest(endpoint: string, apiToken: string): Response =
-    let requestURL = $(parseUri(apiURL) / endpoint)
-    return request(requestURL & "?access_token=" & apiToken)
+proc buildRequest(endpoint: string, apiToken: string): JsonNode =
+    let
+        requestURL = $(parseUri(apiURL) / endpoint)
+        resp = request(requestURL & "?access_token=" & apiToken)
+    return parseJson(resp.body)
 
 proc parseCharacters(apiToken: string): seq[string] =
     let
-        resp = buildRequest("characters", apiToken)
-        payload = parseJson(resp.body)
-    return payload.mapIt(string, $(it.str))
+        payload = buildRequest("characters", apiToken)
+    return payload.mapIt(string, it.str)
 
-proc main(apiToken: string): string =
-    return parseCharacters(apiToken).join(", ")
+proc getCharacterDetails(apiToken: string, characterName: string): string =
+    let
+        payload = buildRequest($(parseUri("characters") / characterName.replace(" ", "%20")), apiToken)
+        name = payload["name"].str
+
+        deaths = payload["deaths"].num
+        age = payload["age"].num
+        avgLifeSpan = age div deaths
+
+        level = payload["level"].num
+        gender = payload["gender"].str
+        race = payload["race"].str
+        profession = payload["profession"].str
+    var
+        res: seq[string] = newSeq[string]()
+    res.add("$# ($#):" % [name, $ level])
+    res.add("\tLevel $# $# $#" % [gender, race, profession])
+    res.add("\tAverage life-span: $#s" % [$avgLifeSpan])
+
+    return res.join("\n")
 
 when isMainModule:
     import os
     var apiToken = ""
     if paramCount() > 0:
-        apiToken = paramStr(1)
-        echo main(apiToken)
+        let apiToken = paramStr(1)
+        if paramCount() > 1:
+            let characterName = paramStr(2)
+            echo getCharacterDetails(apiToken, characterName)
+        else:
+            let characterNames = parseCharacters(apiToken)
+            for characterName in characterNames:
+                echo getCharacterDetails(apiToken, characterName)
