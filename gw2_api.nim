122fc45015630ec6c599762ed1825e8e0e0ebd76
Paul Seidel <pse@ableton.com>
2015-07-09 17:17:01 +0200

    Get item details

--- gw2_api.nim
+++ gw2_api.nim
@@ -1,3 +1,5 @@
+# from tables import TableRef, initTable, add, pairs
+import tables
 from httpclient import request
 from uri import parseUri, `/`, `$`
 from json import items, JsonNode, parseJson, `[]`
@@ -6,20 +8,46 @@ from sequtils import mapIt
 
 const apiURL = "https://api.guildwars2.com/v2/"
 
-proc buildRequest(endpoint: string, apiToken: string): JsonNode =
-    let
+proc buildRequest(endpoint: string, parameters: var Table[string, string], apiToken: string = ""): JsonNode =
+    var
         requestURL = $(parseUri(apiURL) / endpoint)
-        resp = request(requestURL & "?access_token=" & apiToken)
+    if apiToken.len > 0:
+        parameters.add("access_token", $apiToken)
+    if parameters.len > 0:
+        requestURL &= "?"
+        var idx = 1
+        for key, val in parameters.pairs:
+            requestURL &= "$#=$#" % [key, val]
+            if idx != parameters.len:
+                requestUrl &= "&"
+            inc idx
+
+    var resp = request(requestURL)
     return parseJson(resp.body)
+
+proc buildRequest(endpoint: string, apiToken: string = ""): JsonNode =
+    var parameters = initTable[string, string]()
+    return buildRequest(endpoint, parameters, apiToken)
+
+proc getItemDetails(ids: seq[int]): Table[int, string] =
+    let
+        strIds = ids.mapIt(string, $it).join(",")
+    var
+        parameters = {"ids": strIds}.toTable
+    result = initTable[int, string]()
+
+    let payload = buildRequest("items", parameters)
+    for itemNode in payload.elems:
+        result.add(int(itemNode["id"].num), itemNode["name"].str)
 
 proc parseCharacters(apiToken: string): seq[string] =
     let
-        payload = buildRequest("characters", apiToken)
+        payload = buildRequest("characters", apiToken=apiToken)
     return payload.mapIt(string, it.str)
 
 proc getCharacterDetails(apiToken: string, characterName: string): string =
     let
-        payload = buildRequest($(parseUri("characters") / characterName.replace(" ", "%20")), apiToken)
+        payload = buildRequest($(parseUri("characters") / characterName.replace(" ", "%20")), apiToken=apiToken)
         name = payload["name"].str
 
         deaths = payload["deaths"].num
@@ -30,12 +58,25 @@ proc getCharacterDetails(apiToken: string, characterName: string): string =
         gender = payload["gender"].str
         race = payload["race"].str
         profession = payload["profession"].str
+        equipment = payload["equipment"]
+        equipmentDetails = getItemDetails(equipment.mapIt(int, int(it["id"].num)))
     var
         res: seq[string] = newSeq[string]()
-    res.add("$# ($#):" % [name, $ level])
-    res.add("\tLevel $# $# $#" % [gender, race, profession])
-    res.add("\tAverage life-span: $#s" % [$avgLifeSpan])
 
+    res.add("$# ($#):" % [name, $ level])
+    res.add("    Level $# $# $#" % [gender, race, profession])
+    res.add("    Average life-span: $#s" % [$avgLifeSpan])
+
+    res.add("    Equipment:")
+    for equip in equipment:
+        let
+            id = int(equip["id"].num)
+            slot = equip["slot"].str
+            name = equipmentDetails[id]
+        if equipmentDetails.hasKey(id):
+            res.add("        $#: $#" % [slot, name])
+        else:
+            res.add("        $#: $#" % [slot, $ id])
     return res.join("\n")
 
 when isMainModule:
