5daa398b265a89744957455dc5d4a1c855799d91
Paul Seidel <pse@ableton.com>
2015-07-14 17:13:50 +0200

    Add inline qsort for Python

--- qsort.py
+++ qsort.py
@@ -37,27 +37,68 @@ def iter_qsort(it):
             yield n
 
 
+def swap(l, a, b):
+    temp = l[a]
+    l[a] = l[b]
+    l[b] = temp
+
+def qsort_inline(l):
+    stack = []
+    stack.append((0, len(l) - 1))
+    while stack:
+        st = stack.pop()
+        low, high = st
+        pivot_idx = (low + high) / 2
+        while low <= high:
+            while l[low] < l[pivot_idx]:
+                low += 1
+            while l[pivot_idx] < l[high]:
+                high -= 1
+            if low < high:
+                swap(l, low, high)
+                if pivot_idx == low:
+                    pivot_idx = high
+                elif pivot_idx == high:
+                    pivot_idx = low
+                low += 1
+                high -= 1
+            elif low == high:
+                low += 1
+                high -= 1
+                break
+        nlow = low
+        nhigh = high
+        if low > high:
+            nlow = low - 1
+            nhigh = high + 1
+        if nlow - st[0] > 1:
+            stack.append((st[0], nlow))
+        if st[1] - nhigh > 1:
+            stack.append((nhigh, st[1]))
+
+
 if __name__ == "__main__":
-    random_list = [randint(1, 1000) for _ in xrange(1000)]
-
     import time
-    print "qsort0:"
-    start_time = time.time()
-    for _ in range(100):
-        qsort0(random_list)
-    end_time = time.time()
-    print end_time - start_time
+    from functools import wraps
 
-    print "iter_qsort:"
-    start_time = time.time()
-    for _ in range(100):
-        list(iter_qsort(iter(random_list)))
-    end_time = time.time()
-    print end_time - start_time
+    def time_sort_func(sort_func_or_tuple):
+        try:
+            s_func, p_func = sort_func_or_tuple
+            @wraps(s_func)
+            def composite(*args, **kwargs):
+                return p_func(s_func(*args, **kwargs))
+            sort_func = composite
+        except:
+            sort_func = sort_func_or_tuple
+        cumulative_time = 0
+        for _ in range(100):
+            random_list = [randint(1, 1000) for _ in xrange(1000)]
+            start_time = time.time()
+            sort_func(random_list)
+            end_time = time.time()
+            cumulative_time += end_time - start_time
+        return sort_func.__name__, cumulative_time
 
-    print "Sort:"
-    start_time = time.time()
-    for _ in range(100):
-        sorted(random_list)
-    end_time = time.time()
-    print end_time - start_time
+    for sort_func in (qsort, qsort0, (iter_qsort, list), qsort_inline, sorted):
+        name, result = time_sort_func(sort_func)
+        print "{}: {:.4f}s".format(name, result)
